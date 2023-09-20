b3a7bc4005bbe810a85e5c5341c7100dba889d3e
alena.komarova <alena.komarova@rt.ru>
2023-09-20 17:25:47 +0300

    async function

--- Front/lib-project/src/App.tsx
+++ Front/lib-project/src/App.tsx
@@ -13,6 +13,7 @@ import { useSelector, useDispatch} from 'react-redux';
 import { useEffect } from 'react';
 import  * as bookReducers from './store/bookSlice';
 import { books } from './components/data/Books';
+import axios, {AxiosError} from 'axios';
 
 function App() {
   const modalBookDetails = useSelector((state: RootState) => state.bookDetails.visible);
@@ -21,11 +22,56 @@ function App() {
   const modalEditBook = useSelector((state: RootState)=> state.editWindow.visible);
   const modalSearch = useSelector((state:RootState)=>state.search.visibleWindowSearch);
   const dispatch =useDispatch();
-
   const catalog = useSelector((state: RootState)=>state.book.booksCatalog);
 
+async function getUsers() {
+  try{
+    const users = await axios.get('https://fakestoreapi.com/users')
+    console.log(users.data);
+  }
+  catch(e:any){
+    const er = e as AxiosError;
+    console.log(er.message)
+  } 
+  };
+
+  async function putUser(){
+    try{
+      const users = await axios.put('https://fakestoreapi.com/users/7',  {
+        email:'dd@gmail.com',
+        username:'johnd',
+        password:'m38rmF$',
+        name:{
+            firstname:'John',
+            lastname:'Doe'
+        },
+        address:{
+            city:'kilcoole',
+            street:'7835 new road',
+            number:3,
+            zipcode:'12926-3874',
+            geolocation:{
+                lat:'-37.3159',
+                long:'81.1496'
+            }
+        },
+        phone:'1-570-236-7033'
+    })  
+      console.log(users.data);
+    }
+    catch(e:any){
+      const er = e as AxiosError;
+      console.log(er.message)
+    } 
+  };
+  
   useEffect(()=>{
     dispatch(bookReducers.setBook({books}));
+    console.log('get')
+    getUsers();
+    console.log('put')
+    putUser();
+
   },[]);
 
   return (
